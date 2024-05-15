1c9eb44f36c2595cf8ccc285c9e4e9f730a3a12d
Laure Stephan <163327511+TrissCERV@users.noreply.github.com>
2024-05-15 15:55:02 +0200

    Training room

--- Assets/Scripts/SpeciesSwitcher.cs
+++ Assets/Scripts/SpeciesSwitcher.cs
@@ -72,8 +72,7 @@ public class SpeciesSwitcher : MonoBehaviour
 		}
 		if (inTransition)
 		{
-			// textureBlend += Time.deltaTime;
-			textureBlend =0;
+			textureBlend += Time.deltaTime;
 			if(textureBlend==0)
 			{
 				textureBlend = 0;
@@ -98,7 +97,7 @@ public class SpeciesSwitcher : MonoBehaviour
 	public void ApplyPostProcess()
 	{
 		bloom.intensity.value = bloomParams[indexTexture0][lightmapSwitcher.current].intensity;
-		bloom.scatter.value = bloomParams[indexTexture0][lightmapSwitcher.current].scatter;
+		//bloom.scatter.value = bloomParams[indexTexture0][lightmapSwitcher.current].scatter;
 		bloom.tint.value = bloomParams[indexTexture0][lightmapSwitcher.current].tint;
 		colorAdjustments.postExposure.value = postExposure[indexTexture0][lightmapSwitcher.current];
 	}
